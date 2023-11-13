46497814acdbd02eb500372c49a218bf436fedb6
shaghayegh0 <shaghayegh.dehghanis@ryerson.ca>
2023-11-13 22:20:22 +0330

    result style

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # encode images to base64
-    with open("ill_plant.jpg", "rb") as file:
+    with open("plant2.jpg", "rb") as file:
         images = [base64.b64encode(file.read()).decode("ascii")]
 
     response = requests.post(
